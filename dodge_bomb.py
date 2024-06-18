e4217eea0184bb6269d8ed38d3c93fd1af2369ea
koma <c0b23070c6@edu.teu.ac.jp>
2024-06-18 14:49:35 +0900

    練習4 : 衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,25 +5,25 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-DELTA = {  # 移動量辞書
-    pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, +5),
-    pg.K_LEFT: (-5, 0),
-    pg.K_RIGHT: (+5, 0),
+DELTA = { # 移動量辞書
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数：こうかとんRect，または，爆弾Rect
-    戻り値：真理値タプル（横方向，縦方向）
-    画面内ならTrue／画面外ならFalse
+    引数：こうかとんrct, 爆弾rct
+    戻り地：真理値タプル(横方向, 縦方向)
+    画面内ならTrue, 画面内ならFalse
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+    if rct.left < 0 or WIDTH < rct.right:
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
 
@@ -35,18 +35,20 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
-    bb_img = pg.Surface((20, 20))  # 1辺が20の空のSurfaceを作る
+    bb_img = pg.Surface((20, 20)) # 1辺が20の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #からのSurfaceに赤い円を描く
+    bb_rct = bb_img.get_rect()
+    bb_rct.center =random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = 5, 5 #　爆弾の横縦速度ベクトル
     bb_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 空のSurfaceに赤い円を描く
-    bb_rct = bb_img.get_rect()  # 爆弾Rect
-    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    vx, vy = +5, +5  # 爆弾の横方向速度，縦方向速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+        if kk_rct.colliderect(bb_rct): # 衝突判定
+            return #ゲームオーバー
         screen.blit(bg_img, [0, 0])
 
         key_lst = pg.key.get_pressed()
@@ -59,18 +61,16 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-
         bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  # 横方向にはみ出たら
+        if not yoko:
             vx *= -1
-        if not tate:  # 縦方向にはみ出たら
+        if not tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
